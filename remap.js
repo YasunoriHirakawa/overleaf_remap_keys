a5f5c842b104dd063de98d2e629a8fd6f0e322d0
YasunoriHirakawa <area51muepon1ichi@gmail.com>
2021-08-02 11:57:21 +0900

    Add script

---
+++ remap.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Overleaf Editor Custom VIM Keybindings
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @match        https://www.overleaf.com/project/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // poll until editor is loaded
+    const retry = setInterval(() => {
+        if (window._debug_editors === undefined) return
+        clearInterval(retry)
+        // get current editor instance
+        const editor = window._debug_editors[window._debug_editors.length -1]
+        // vim keyboard plugin
+        const vimKeyboard = window.ace.require("ace/keyboard/vim")
+        // add custom keybindings - insert mode applies on insert
+        vimKeyboard.Vim.map("jj", "<Esc>", "insert")
+        // normal mode applies while escaped
+        vimKeyboard.Vim.map("h", "j", "normal")
+        // set the modified keyboard handler for editor
+        editor.setKeyboardHandler(vimKeyboard.handler)
+        console.log("Custom key bindings applied")
+    }, 100)
+})();
